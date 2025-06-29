c7deec538d362d6f34eb72aeee65da456e595052
Dhruvang-cyg <154967777+Dhruvang-cyg@users.noreply.github.com>
2025-06-29 16:04:14 +0530

    Add files via upload

---
+++ gem/README.md
@@ -0,0 +1,7 @@
+## Gem
+
+Government e Marketplace
+
+#### License
+
+mit
---
+++ gem/gem/gem/doctype/gem_item_table/gem_item_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Dhruvang Darji and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class GemItemTable(Document):
+	pass
